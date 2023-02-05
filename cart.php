07d5c0f930292b217f6e1813c40b1cbf4a25e44d
Pieta9221 <mercy_nwaodu@yahoo.com>
2023-02-06 00:12:15 +0100

    add

---
+++ cart.php
@@ -0,0 +1,180 @@
+<?php
+session_start();
+require 'connection.php';
+$config = new mysqli ($host, $user, $pwd, $database);
+
+if(!isset($_SESSION['admin'])){
+header("location: signin.php"); 
+}
+
+?> 
+<?php
+$email = $_SESSION['admin']; 
+$query3  = "SELECT * FROM admindata WHERE email = '$email'";
+$res3 = $config->query($query3);
+$row = $res3->fetch_array();
+?>
+<?php
+  $url = $_SERVER['PHP_SELF'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Responsive Bootstrap4 Shop Template, Created by Imran Hossain from https://imransdesign.com/">
+
+	<!-- title -->
+	<title>Foodie</title>
+
+	<!-- favicon -->
+	<link rel="shortcut icon" type="image/png" href="assets/img/favicon.png">
+	<!-- google font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+	<!-- fontawesome -->
+	<link rel="stylesheet" href="assets/css/all.min.css">
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<!-- owl carousel -->
+	<link rel="stylesheet" href="assets/css/owl.carousel.css">
+	<!-- magnific popup -->
+	<link rel="stylesheet" href="assets/css/magnific-popup.css">
+	<!-- animate css -->
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<!-- mean menu css -->
+	<link rel="stylesheet" href="assets/css/meanmenu.min.css">
+	<!-- main style -->
+	<link rel="stylesheet" href="assets/css/main.css">
+	<!-- responsive -->
+	<link rel="stylesheet" href="assets/css/responsive.css">
+
+</head>
+<body>
+	
+	<!--PreLoader-->
+    <div class="loader">
+        <div class="loader-inner">
+            <div class="circle"></div>
+        </div>
+    </div>
+    <!--PreLoader Ends-->
+	
+	<!-- header -->
+	<div class="top-header-area" id="sticker">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-sm-12 text-center">
+					<div class="main-menu-wrap">
+						<!-- logo -->
+						<div class="site-logo">
+							<a href="index.php">
+								<img src="assets/img/logo.png" alt="">
+							</a>
+						</div>
+						<!-- logo -->
+
+								<!-- menu start -->
+                <nav class="main-menu">
+							<ul>
+              <li><a href="index.php" class="<?php echo str_ends_with($url, '/index.php') ? 'current-list-item' : '' ?>"> Home</a></li>
+        			<li><a href="about.php" class="<?php echo str_ends_with($url, '/about.php') ? 'current-list-item' : '' ?>"> <span>About</span></a></li>
+              <li><a href="contact.php" class="<?php echo str_ends_with($url, '/contact.php') ? 'current-list-item' : '' ?>"> Contact</a></li>
+              <li><a href="shop.php" class="<?php echo str_ends_with($url, '/shop.php') ? 'current-list-item' : '' ?>"> Shop</a></li>
+								
+								</li>
+								<?php
+if(isset($_SESSION['user'])){
+
+?><li><a href="profile.php" class="<?php echo str_ends_with($url, '/profile.php') ? 'current-list-item' : '' ?>"> Profile</a></li>
+								
+</li>
+								<li>
+									<div class="header-icons">
+										
+										<a class="<?php echo str_ends_with($url, '/logout.php') ? 'current-list-item' : '' ?>" href="logout.php"><i class="fas fa-power-off"></i> Logout</a>
+										</div>
+								</li>
+							</ul><?php
+}?>
+						</nav>
+												<div class="mobile-menu"></div>
+						<!-- menu end -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end header -->
+
+	<!-- end search arewa -->
+	
+	<!-- breadcrumb-section -->
+	<div class="breadcrumb-section breadcrumb-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 text-center">
+					<div class="breadcrumb-text">
+						<p>fill your cart with amazing treats</p>
+						<h1>Cart</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end breadcrumb section -->
+
+	<!-- cart -->
+	<div class="cart-section mt-150 mb-150">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-md-12">
+					<div class="cart-table-wrap">
+						<table class="cart-table">
+							<thead class="cart-table-head">
+								<tr class="table-head-row">
+									
+									<th class="product-name">ID</th>
+									<th class="product-name">Username</th>
+									<th class="product-name">Email</th>
+									<th class="product-name">Phone</th>
+									<th class="product-name">Address</th>
+									
+								</tr>
+							</thead>
+							<tbody>
+                <?php
+                $query2  = "SELECT * FROM users ORDER BY username ASC";
+                $result2 = $config->query($query2);
+                if($result2->num_rows == 0){
+                  echo "Data not found";
+                }else{
+                  while($row = $result2 -> fetch_array()){
+                    echo "<tr class='table-body-row'>";
+                    echo "<td class='product-name'>".$row['userid']."</td>";
+                    echo "<td class='product-name'>".$row['username']."</td>";
+                    echo "<td class='product-name'>".$row['email']."</td>";
+                    echo "<td class='product-name'>".$row['phone']."</td>";
+                    echo "<td class='product-name'>".$row['address']."</td>";
+									
+                  echo "</tr>";
+                }
+                }
+                ?>  
+								
+							</tbody>
+						</table>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<!-- end cart -->
+
+			<!-- footer -->
+			<?php
+include ('copyright.php');
+?>
